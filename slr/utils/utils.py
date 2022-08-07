64fe1608b6d8d9217493f9df7a031448bed8e028
0hyunU <ryh2803@gmail.com>
2022-08-08 01:46:16 +0900

    U

--- slr/utils/utils.py
+++ slr/utils/utils.py
@@ -7,16 +7,17 @@ import pytest
 
 def only_test_on_windows(func):
 
-    def wrapper(*args):
-        return pytest.mark.skipif(platform != "win32", reason="only available windows desktop")(func)
+    def wrapper(*args,**kwargs):
+        return pytest.mark.skipif(platform != "win32", reason="only available windows desktop")(func(*args,**kwargs))
     
     return wrapper
 
-def get_tensorboard_callback():
+def get_tensorboard_callback(more_info = None):
     import datetime
     import tensorflow as tf
 
     log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    if more_info : log_dir = log_dir + "_" + more_info
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
     return tensorboard_callback
 
