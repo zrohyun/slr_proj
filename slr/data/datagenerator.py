c7541a04b15a35fea19ae58cd787280eebc0b1d8
0hyunU <ryh2803@gmail.com>
2022-08-08 01:45:24 +0900

    keypoint generator

--- slr/data/datagenerator.py
+++ slr/data/datagenerator.py
@@ -14,7 +14,7 @@ from sklearn.preprocessing import MinMaxScaler as MMS
 # and `y_set` are the associated classes.
 class KeyDataGenerator(Sequence):
     'Generates data for Keras'
-    def __init__(self, x_set, y_set, batch_size=32, seq_len = 200, shuffle=True, scale=False):
+    def __init__(self, x_set, y_set, batch_size=32, seq_len = 200, shuffle=True, scale=True):
         'Initialization'
         self.x_set, self.y_set = x_set, y_set
         self.batch_size = batch_size
@@ -32,23 +32,23 @@ class KeyDataGenerator(Sequence):
 
     def __getitem__(self, index):
         'Generate one batch of data'
-        # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        # Generate indices of the batch
+        indices = self.indices[index*self.batch_size:(index+1)*self.batch_size]
 
         # Find list of IDs
-        y: List[int] = self._label_encode(indexes)
-        X: np.ndarray = self._get_data_with_zero_pad(indexes)
+        y: List[int] = self._label_encode(indices)
+        X: np.ndarray = self._get_data_with_zero_pad(indices)
         
         return X, to_categorical(y,num_classes=self.n_classes).argmax(axis=1)
 
     def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.x_set))
+        'Updates indices after each epoch'
+        self.indices = np.arange(len(self.x_set))
         if self.shuffle == True:
-            np.random.shuffle(self.indexes)
+            np.random.shuffle(self.indices)
 
-    def _label_encode(self, indexes):
-        labels = [self.y_set[k] for k in indexes]
+    def _label_encode(self, indices):
+        labels = [self.y_set[k] for k in indices]
         return [self.cls_map[l] for l in labels]
 
     def _minmax4dim(self,X,eps = 1e-10):
@@ -58,15 +58,19 @@ class KeyDataGenerator(Sequence):
         X = np.array(((X - Xmin) / (Xmax - Xmin)))
         return X
     
-    def _get_data_with_zero_pad(self, indexes) -> np.ndarray:
-        X = [KeypointSeq(self.x_set[k]).key_arr for k in indexes]
+    def _get_data_with_zero_pad(self, indices) -> np.ndarray:
+        X = self._read_data(indices)
+        X = X.reshape(*X.shape[:-2],-1) #flatten feature
+        # X.shape = (batch_size, seq_len, 137s)
+        return X
+
+    def _read_data(self, indices) -> np.ndarray:
+        X = [KeypointSeq(self.x_set[k]).key_arr for k in indices]
         X = np.array([zp(x,self.seq_len) for x in X])
         
         if self.scale:
             X = self._minmax4dim(X)
-        X = X.reshape(*X.shape[:-2],-1) #flatten feature
-        # X.shape = (batch_size, seq_len, 137s)
-
+        
         return X
 
 if __name__ == '__main__':
