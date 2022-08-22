5965631e728865010c7f5434cd0628fd1cb6b181
0hyunU <ryh2803@gmail.com>
2022-08-23 00:31:24 +0900

    Merge DataGenerators

--- slr/data/datagenerator.py
+++ slr/data/datagenerator.py
@@ -1,14 +1,18 @@
-import time
 from slr.data.ksl.datapath import DataPath
-import numpy as np
-import math
+from slr.data.ksl.keypoint_json_parser import KeypointSeq
+from slr.data.ksl.graphseq import GraphSeq
 from slr.utils.utils import zero_pad_keypoint_seq as zp
 
+
+import numpy as np
+from scipy.spatial.distance import cdist
+
+import tensorflow as tf
+from tensorflow.keras.utils import Sequence, to_categorical
+
+import time
 from typing import List
 
-from tensorflow.keras.utils import Sequence, to_categorical
-from slr.data.ksl.keypoint_json_parser import KeypointSeq
-from sklearn.preprocessing import MinMaxScaler as MMS
 
 # Here, `x_set` is list of path to the images
 # and `y_set` are the associated classes.
@@ -78,6 +82,128 @@ class KeyDataGenerator(Sequence):
         X = np.array([zp(x, self.seq_len) for x in X])
 
         return X
+
+
+class GraphDataGenerator(KeyDataGenerator):
+    """
+    Data Generator for Keras
+    Here, `x_set` is list of path to the Adj Matrix
+    and `y_set` are the associated classes.
+    """
+
+    def __init__(
+        self, x_set, y_set, batch_size=32, seq_len=200, shuffle=True, scale=False
+    ):
+        "Initialization"
+        super().__init__(x_set, y_set, batch_size, seq_len, shuffle, scale)
+
+    def __getitem__(self, index):
+        indices = self.indices[index * self.batch_size : (index + 1) * self.batch_size]
+
+        y: List[int] = self._label_encode(indices)
+        # X = self._read_data(indices)
+        X = self._get_data(indices)
+        return X, to_categorical(y, num_classes=self.n_classes).argmax(axis=1)
+
+    def _read_data(self, indices) -> np.ndarray:
+        X = [GraphSeq(self.x_set[k]).graph_mat for k in indices]
+        X = np.array([zp(x, self.seq_len) for x in X])
+
+        return X
+
+    def _get_data(self, indices) -> np.ndarray:
+        X = self._read_data(indices)
+        # X = self._adj_mat(X)
+        return X
+
+    def _adj_mat(self, X):
+        from sklearn.metrics import pairwise_distances
+
+        delta = 0.5
+        A = np.stack(
+            [
+                np.stack(
+                    [
+                        self._normalize_undigraph(
+                            np.exp(
+                                -cdist(X[i, w], X[i, w], metric="euclidean")
+                                / (2.0 * delta**2)
+                            )
+                        )
+                        # self._normalize_undigraph(np.exp(- 1./(2 * 1) * pairwise_distances(X[i,w], metric='sqeuclidean')))
+                        for w in range(200)
+                    ]
+                )
+                for i in range(self.batch_size)
+            ]
+        )
+        return A
+
+    def _normalize_undigraph(self, A):
+        Dl = np.sum(A, 0)
+        num_node = A.shape[0]
+        Dn = np.zeros((num_node, num_node))
+        for i in range(num_node):
+            if Dl[i] > 0:
+                Dn[i, i] = Dl[i] ** (-0.5)
+        DAD = np.dot(np.dot(Dn, A), Dn)
+        return DAD
+
+
+class TFRecDataGenerator:
+    def __init__(self, tfrec_file, comp=None, batch_size=32):
+        self.dataset = self._get_ksl_dataset_from_tfrec(tfrec_file, comp, batch_size)
+        self._iterator = self.dataset
+
+    def __iter__(self):
+        if self._iterator is None:
+            self._iterator = iter(self.dataset)
+        else:
+            self._reset()
+
+        return self._iterator
+
+    def _reset(self):
+        self._iterator = iter(self.dataset)
+
+    def __next__(self):
+        batch = next(self._iterator)
+        # return batch
+        batch = self._parse_batch(batch)
+        return batch["raw_data"], batch["label"]
+
+    def _parse_batch(self, batch):
+        batch["label"] = np.array(list(map(lambda x: int(x.decode()), batch["label"])))
+        return batch
+
+    def _get_ksl_dataset_from_tfrec(self, file, comp, batch_size, channel=2):
+        dataset = tf.data.TFRecordDataset(file, comp)
+        parsed_dataset = self._get_parsed_dataset(dataset, batch_size, channel)
+        parsed_dataset = parsed_dataset.as_numpy_iterator()
+        # tfds.as_numpy(parsed_dataset)
+        return parsed_dataset
+
+    def _get_parsed_dataset(
+        self, tfrec_Dset: tf.data.TFRecordDataset, batch_size, channel
+    ):
+        image_feature_description = {
+            "raw_data": tf.io.FixedLenFeature([], tf.string),
+            "data_shape": tf.io.FixedLenFeature([], tf.string),
+            "label": tf.io.FixedLenFeature([], tf.string),
+        }
+
+        def _parse_image_function(example):
+            example = tf.io.parse_single_example(example, image_feature_description)
+            # example['label'] = tf.cast(example['label'],tf.string)
+            # raise Exception(f'{tf.io.decode_raw(example["label"],tf.int16)}')
+            example["raw_data"] = tf.reshape(
+                tf.io.decode_raw(example["raw_data"], tf.float32), (-1, 137, channel)
+            )
+            return example
+
+        parsed_dataset = tfrec_Dset.map(_parse_image_function).batch(batch_size)
+        parsed_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        return parsed_dataset
 
 
 if __name__ == "__main__":
